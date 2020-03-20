cec042f9facaabd75cd9954e2699c98d5f368c37
Milan Dojchinovski <dojcinovski.milan@gmail.com>
2020-03-20 14:52:17 +0100

    sparql update

--- functions.sh
+++ functions.sh
@@ -104,11 +104,11 @@ function prepare_datasets_query ()
 	
 	# Get all files
 	SELECT DISTINCT ?distribution ?file WHERE {
-		<http://dbpedia-generic.tib.eu/release/text/equations/2020.02.01/dataid.ttl#Dataset> dataid:group <https://databus.dbpedia.org/"$1"> .
-		<http://dbpedia-generic.tib.eu/release/text/equations/2020.02.01/dataid.ttl#Dataset> dct:hasVersion \""$2"\"^^xsd:string .
-		<http://dbpedia-generic.tib.eu/release/text/equations/2020.02.01/dataid.ttl#Dataset> dcat:distribution ?distribution .
+		?dataset dataid:group <https://databus.dbpedia.org/"$1"> .
+		?dataset dct:hasVersion \""$2"\"^^xsd:string .
+		?dataset dcat:distribution ?distribution .
 		?distribution dcat:downloadURL ?file .
-	} LIMIT 2"
+	}"
 }
 
 
